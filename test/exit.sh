0ba935341a6b49ec0614de59c27ba41393250c10
Yun Li <yli132@ucr.edu>
2017-05-31 21:25:01 -0700

    Update exit.sh

--- test/exit.sh
+++ test/exit.sh
@@ -1,5 +1,14 @@
-echo hello
-ls
-mkdir file
+#!/bin/sh
+ls || exit
+(ls || echo Hello) && echo world && exit
+ls -a && (echo Hello && exit)
+mkdir testcase
+test -e testcase || exit
+test -d testcase && exit
+[ -f testcase ] || exit
+[ -e testcase] && exit
+ls -a && (echo Hello && exit)
+(exit && echo Hello) || (echo world  && echo nice)
+(echo Hello && echo world) || nice || exit
+(echo Hello && echo world) && nice || exit
 exit
-
