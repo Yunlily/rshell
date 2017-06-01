bd73e190b2cc4a76b0d193fdadf0e90ab3826600
Yun Li <yli132@ucr.edu>
2017-05-31 21:25:33 -0700

    Update multi_command.sh

--- test/multi_command.sh
+++ test/multi_command.sh
@@ -1,6 +1,24 @@
 #!/bin/sh
--a; echo hello && mkdir test || echo world; git status
-; ls && echo world
-;ls && echo world
-||ls || ls
+test -e /test/file/path
+[ -e /test/file/path ]
+test -f /test/file/path
+[ -f /test/file/path ]
+test -d /test/file/path
+[ -d /test/file/path ]
+test -e /test/file/path && echo Hello world
+[ -e /test/file/path ] && echo Hello world
+test -f /test/file/path && echo Hello world
+[ -f /test/file/path ] && echo Hello world
+test -d /test/file/path && echo Hello world
+[ -d /test/file/path ] && echo Hello world
+test -e /test/file/path || echo Hello world
+[ -e /test/file/path ] || echo Hello world
+test -f /test/file/path || echo Hello world
+[ -f /test/file/path ] || echo Hello world
+test -d /test/file/path || echo Hello world
+[ -d /test/file/path ] || ls -a && echo Hello world
+echo Hello world && ls -a || [ -d /test/file/path ]
+echo Hello world && nice || ls -a
+echo Hello world || nice && ls -a
+ls -a && test -e /test/file/path || echo Hello world
 exit
